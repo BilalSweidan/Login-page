13706fe9243c91533ab116d99d0dd546a856e610
BilalSweidan <bilalsweidan58@gmail.com>
2024-01-20 11:32:20 +0300

    added project structure

---
+++ admin.js
@@ -0,0 +1,21 @@
+// function show(){
+//     let image = document.getElementById("eye")
+//     console.log(image.src)
+//     //let showpassword = document.getElementsByClassName("username2")
+//     //console.log(showpassword.type)
+//     if (image.src = 'open-eye.ico'){
+//         image.src = 'close-eye.ico'
+//         console.log(image.src)
+//     }else 
+//     {
+//         image.src = 'open-eye.ico'
+//         console.log(image.src)
+//     };
+        
+// }
+function show(){
+    let password = document.getElementsByName("username2")
+    password.setAttribute("type") = "text"
+
+    
+}
